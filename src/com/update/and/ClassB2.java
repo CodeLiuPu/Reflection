a24d9384f40782f13715cdcc221497f99aa4334c
liupu <575866447@qq.com>
2019-06-21 00:11:25 +0800

    ClassB2

---
+++ src/com/update/and/ClassB2.java
@@ -0,0 +1,15 @@
+package com.update.and;
+
+/**
+ * @author : liupu.
+ * date : 2019/06/20
+ * desc :
+ */
+public class ClassB2 implements ClassB2Interface {
+    public int id;
+
+    @Override
+    public void doSomething() {
+        System.out.println("doSomething");
+    }
+}
---
+++ src/com/update/and/ClassB2Interface.java
@@ -0,0 +1,10 @@
+package com.update.and;
+
+/**
+ * @author : liupu.
+ * date : 2019/06/20
+ * desc :
+ */
+public interface ClassB2Interface {
+    void doSomething();
+}
